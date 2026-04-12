cf95e59f2192d6b798bc5844c963a19a938e1a79
Wellington Jesus <wellannaflla@gmail.com>
2026-04-12 08:50:01 -0300

    Atualizar o app.py
    coloquei a função de excluir e editar para ADM.

--- app.py
+++ app.py
@@ -48,6 +48,15 @@ def sucesso():
 @app.route('/admin')
 @auth.login_required
 def admin():
+
+@app.route('/excluir/<int:id>')
+@auth.login_required
+def excluir(id):
+    convidado = Convidado.query.get_or_404(id)
+    db.session.delete(convidado)
+    db.session.commit()
+    return redirect(url_for('admin'))
+
     todos = Convidado.query.all()
     total = sum([c.acompanhantes + 1 for c in todos])
     return render_template('admin.html', convidados=todos, total=total)
